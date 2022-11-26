3142ef00b0dc393ad5de384c697dd0c99951eeb0
Zupiter4Earth3 <117378185+Zupiter4Earth3@users.noreply.github.com>
2022-11-25 23:53:18 -0500

    This is required

---
+++ src/launchBrowser/LaunchChrome.java
@@ -0,0 +1,21 @@
+package launchBrowser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "drive\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("q")).sendKeys("selenium");
+		driver.findElement(By.name("btnK")).click();
+		Thread.sleep(4000);
+		driver.close();
+
+	}
+
+}
